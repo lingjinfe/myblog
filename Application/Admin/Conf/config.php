0e5eb3068a3da19c6197d93cf48915e29ae4917e
lingjinfe <15260771518@163.com>
2020-07-30 16:16:55 +0800

    1.1.2

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,4 +1,9 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'TMPL_PARSE_STRING'	=>array(
+	"__PUBLIC__" =>SITE_URL."/Application/Admin/Public",//更改默认的public替换规则
+),
+	//"__JS__"	 =>"/Public/JS/",//增加新的JS类库路径替换规则
+	//"__UPLOAD__" =>"/Uploads",//增加新的上传路径替换规则
 );
--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -1,8 +1,8 @@
 <?php
-namespace Home\Controller;
+namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->show(123);
+        $this->display();
     }
 }
