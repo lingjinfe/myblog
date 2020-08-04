20b09d28db8ac69cf99628d234bd82e563a9103a
lingjinfe <15260771518@163.com>
2020-08-05 00:38:32 +0800

    1.1.3

---
+++ Application/Admin/Controller/CateController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class CateController extends Controller {
+    public function lst(){
+        $this->display();
+    }
+    public function add(){
+        $this->display();
+    }
+    public function edit(){
+        $this->display();
+    }
+    public function del(){
+
+    }
+}
